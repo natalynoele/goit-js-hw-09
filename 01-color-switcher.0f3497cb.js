c518e7df87a0de6c1806086141c98164e493af01
natalynoele <46425788+natalynoele@users.noreply.github.com>
2023-01-23 18:45:36 +0000

    Deploying to gh-pages from @ natalynoele/goit-js-hw-09@cc2550ea4f4ea7399202bcfc7eac3f2d18c1a879 🚀

---
+++ 01-color-switcher.0f3497cb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.classList.add("switcher-start"),e.classList.add("switcher-stop"),t.disabled=!1,e.disabled=!0,t.addEventListener("click",(function(){d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.0f3497cb.js.map
